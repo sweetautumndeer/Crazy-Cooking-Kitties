567d3f1258ee2ec585e861b3a9daef4f5fa1c59a
sweetautumndeer <amoulios@yahoo.com>
2021-09-15 19:07:39 -0700

    Added Functioning Main Menu
    The main menu now features 4 new buttons! Start, Options, Extras, and Exit.

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+
+    public GameObject MainPanel;
+    public GameObject LevelSelectPanel;
+    public GameObject OptionsPanel;
+    public GameObject BingusPanel;
+
+    public Scene Level1;
+
+    // Start is called before the first frame update
+    void Start() {
+        MainPanel.SetActive(true);
+        LevelSelectPanel.SetActive(false);
+        OptionsPanel.SetActive(false);
+        BingusPanel.SetActive(false);
+    }
+
+    #region Level Select
+    public void ShowLevelSelect() {
+        MainPanel.SetActive(false);
+        LevelSelectPanel.SetActive(true);
+    }
+
+    public void HideLevelSelect() {
+        LevelSelectPanel.SetActive(false);
+        MainPanel.SetActive(true);
+    }
+
+    public void LoadLevel1() {
+        SceneManager.LoadScene("RhythmTesting");
+    }
+    #endregion
+
+    public void ShowOptions() { OptionsPanel.SetActive(true); }
+    public void HideOptions() { OptionsPanel.SetActive(false); }
+
+    public void ToggleBingus() { 
+        if (BingusPanel.activeSelf)
+            BingusPanel.SetActive(false);
+        else
+            BingusPanel.SetActive(true);
+    }
+
+    public void QuitGame() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
