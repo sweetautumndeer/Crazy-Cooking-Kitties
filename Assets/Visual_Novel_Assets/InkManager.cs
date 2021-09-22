da378f9338119791835e32e724900f0cf2e78fbf
SteeRevo <69092521+SteeRevo@users.noreply.github.com>
2021-09-21 18:43:32 -0700

    basic text advancements

---
+++ Assets/Visual_Novel_Assets/InkManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+using UnityEngine.UI;
+
+public class InkManager : MonoBehaviour
+{
+    [SerializeField]
+  private TextAsset _inkJsonAsset;
+  private Story _story;
+
+  [SerializeField]
+  private Text _textField;
+
+  void Start()
+  {
+    StartStory();
+  }
+
+  private void StartStory()
+  {
+    _story = new Story(_inkJsonAsset.text);
+    DisplayNextLine();
+  }
+  
+  public void DisplayNextLine()
+  {
+    if (!_story.canContinue) return;
+    
+    string text = _story.Continue(); // gets next line
+    text = text?.Trim(); // removes white space from text
+    _textField.text = text; // displays new text
+  }
+}
---
+++ Assets/Visual_Novel_Assets/inkTest.ink
@@ -0,0 +1,4 @@
+Ethan: Hello, this is the start of the story
+Humble Ethan: Wow, this is great
+Ethan: I know, its perfect.
+    -> END
